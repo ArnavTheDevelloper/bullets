d917fd429702a31b5dc3a9e8c6aec285688f0b51
ArnavTheDevelloper <64299098+ArnavTheDevelloper@users.noreply.github.com>
2020-05-03 10:19:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+
+var wall,bullet
+
+var thickness,speed,weight
+
+function setup(){
+
+createCanvas(1300, 400);
+
+thickness=random(22,83)
+
+wall=createSprite(1200,200,thickness,height/2)
+
+
+
+}
+
+function draw(){
+
+
+
+
+
+
+
+	drawSprites;
+}
